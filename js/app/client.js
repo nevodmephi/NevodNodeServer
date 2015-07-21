b1806c3607339ecd759f5068acfcd10e7c2050e4
mephist <mephist@twist>
2015-07-21 14:40:46 +0300

    First commit

---
+++ js/app/client.js
@@ -0,0 +1,516 @@
+var socket;
+
+var selectedConnection;
+var instance;
+
+var curConnection;
+
+var curid=0;
+
+var plot;
+var oscScreen;
+
+var oscPlay = true;
+
+function initjsPlumb() {
+    instance = jsPlumb.getInstance({
+        Endpoint: ["Dot", {radius: 1}],
+        HoverPaintStyle: {strokeStyle: "#333333", lineWidth: 1 },
+        ConnectionOverlays: [
+            [ "Arrow", {
+                location: 1,
+                id: "arrow",
+                length: 14,
+                foldback: 0.9
+            } ],
+            [ "Label", { label: "", id: "label", cssClass: "aLabel" }]
+        ],
+        Container: "statemachine-demo"
+    });
+    window.jsp = instance;
+    var windows = jsPlumb.getSelector(".statemachine-demo .w");
+    instance.bind("click", function (c) {
+        var f = "";
+        var t = "";
+        document.getElementById("inputsFrom").innerHTML = "";
+        document.getElementById("inputsTo").innerHTML = "";
+        /*switch(c.source._type) {
+            case "SER":
+                f += formStr("f","A");
+                break;
+            case "MOD":
+                f += formStr("f","Checked");
+                f += formStr("f","True");
+                f += formStr("f","False");
+                break;
+            case "COU":
+                f += formStr("f","Y1");
+                f += formStr("f","Y2");
+                break;
+            case "SRC":
+                f += formStr("f","Event");
+                break;
+            case "FIL":
+                f += formStr("f","Event");
+                break;
+        }*/
+        f += formStr("f","A");
+        f += formStr("f","B");
+        f += formStr("f","C");
+        document.getElementById("inputsFrom").innerHTML = f;
+        /*switch(c.target._type) {
+            case "SER":
+                t += formStr("t","Y");
+                t += formStr("t","X");
+                break;
+            case "MOD":
+                t += formStr("t","X");
+                t += formStr("t","Y");
+                break;
+            case "COU":
+                t += formStr("t","Checked");
+                t += formStr("t","True");
+                t += formStr("t","False");
+                break;
+            case "SRC":
+                break;
+            case "FIL":
+                break;
+        }*/
+        t += formStr("t","A");
+        t += formStr("t","B");
+        t += formStr("t","C");
+        document.getElementById("inputsTo").innerHTML = t;
+        var l = c.getOverlay('label').getLabel();
+        if(l!="not set") {
+            document.getElementById("f-" + l.split(" ")[0]).className += " active";
+            document.getElementById("t-" + l.split(" ")[2]).className += " active";
+        }
+        $('#connectionSettings').modal('show');
+        curConnection = c;
+    });
+    instance.bind("connection", function (info) {
+        info.connection.getOverlay('label').setLabel("not set");
+    });
+    instance.batch(function () {
+        instance.makeSource(windows, {
+            filter: ".ep",
+            anchor: "Continuous",
+            connector: [ "StateMachine", { curviness: 10 } ],
+            connectorStyle: { strokeStyle: "#333333", lineWidth: 1, outlineColor: "transparent", outlineWidth: 10 },
+            maxConnections: 5,
+            onMaxConnections: function (info, e) {
+                alert("Maximum connections (" + info.maxConnections + ") reached");
+            }
+        });
+        instance.makeTarget(windows, {
+            dropOptions: { hoverClass: "dragHover" },
+            anchor: "Continuous",
+            allowLoopback: true
+        });
+    });
+    jsPlumb.fire("jsPlumbDemoLoaded", instance);
+}
+
+function formStr(t, name) {
+    return ('<label class="btn btn-warning ' + t + '-input' + '" id="' + t + '-' + name + '"><input type="radio" name="options" autocomplete="off">' + name + '</label>');
+}
+
+function deleteConnection() {
+    jsPlumb.detach(curConnection);
+}
+
+function setConnection() {
+    var from; var to;
+    $( ".btn.btn-warning.f-input.active" ).each(function(i,o) {
+        from = o.id.split("-")[1];
+    });
+    $( ".btn.btn-warning.t-input.active" ).each(function(i,o) {
+        to = o.id.split("-")[1];           
+    });
+    if((from!=undefined)&&(to!=undefined)) {
+        curConnection.getOverlay('label').setLabel(from + " &rarr; " + to);
+    }
+}
+
+function newBlock(type) {
+    var newAddon = document.createElement('div');
+    newAddon.className = "w";
+    newAddon.id = "block" + curid;
+    newAddon._flag = true;
+    newAddonPanel = document.createElement('div');
+    newAddonPanel.className = "panel panel-default";
+    newAddonPanelHeading = document.createElement('div');
+    newAddonPanelSettingsButton = document.createElement('button');
+    newAddonPanelHeading.className = "panel-heading";
+
+    newAddonPanelHeadingButton = document.createElement('button');
+    newAddonPanelHeadingButton.className = "ep btn btn-warning btn-xs";
+    newAddonPanelHeadingButton.innerHTML = "&nbsp;";    
+    newAddonPanelSettingsButton.className = "btn btn-success btn-xs";
+    newAddonPanelSettingsButton.innerHTML = "&nbsp;";    
+    //newAddon.innerHTML = '<div class="panel panel-default"><div class="panel-heading"><button class="ep btn btn-warning btn-xs"><span class="ep glyphicon glyphicon-flash"></span></button>&nbsp;<a target="_blank" href="' + data.url + '"><button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-fullscreen"></span></button></a>&nbsp; ' + data.title + '<button onclick="$(\'#addonSettingsDialog\').modal({show: true});" class="btn btn-danger btn-xs pull-right"><span class="glyphicon glyphicon-wrench"></span></button></div><div class="panel-body"><div id="' + data.id + '-canvas" style="width:' + data.width + 'px;height:' + data.height + 'px;"></div></div></div>';
+    newAddonPanelHeadingHeader = document.createElement('span');
+
+    var hd;
+    var ap; var bp; var gp;
+    switch(type) {
+        case "SER":                           
+            newAddon._type = 2;     //SERVER
+            hd = "Сервер";
+            ap = "порог А";
+            bp = "порог B";
+            cp = "порог C";
+            break;
+        case "RND":
+            newAddon._type = 1;     //Randomize
+            hd = "Случ. соб.";
+            ap = "ниж. гр.";
+            bp = "верх. гр.";
+            cp = "интервал";
+            break;
+        case "COU":
+            newAddon._type = 3; 
+            hd = "Счетчик";
+            ap = "начало";
+            bp = "ширина";
+            cp = "число";
+            break;
+        case "SRC":
+            hd = "Источник";
+            ap = "библиотека";
+            bp = "параметр1";
+            cp = "параметр2";
+            break;
+        case "FIL":
+            newAddon._type = 3;
+            hd = "Файл";
+            ap = "путь";
+            bp = "интервал";
+            cp = "пакетов";
+            break;
+    }
+    newAddonPanelHeadingHeader.innerHTML = "&nbsp;" + hd;
+
+    newAddonPanelHeading.appendChild(newAddonPanelHeadingButton);
+    newAddonPanelHeading.appendChild(newAddonPanelSettingsButton);
+    newAddonPanelHeading.appendChild(newAddonPanelHeadingHeader);
+    newAddonPanel.appendChild(newAddonPanelHeading);
+
+    newAddon.style.left = document.body.scrollLeft + window.innerWidth/2 - 300/2 + getRandomInt(-25,25) +"px";
+    newAddon.style.top = document.body.scrollTop + window.innerHeight*0.6 + getRandomInt(-25,25) + "px";
+
+    newAddon.style.width = 160 + "px";
+
+    newAddon.appendChild(newAddonPanel);
+
+    var newAddonTextbox = document.createElement('input');
+    newAddonTextbox.placeholder = ap;
+    newAddonTextbox.className = "form-control";
+    newAddonTextbox.id = "block" + curid + "-alpha";
+    newAddonPanel.appendChild(newAddonTextbox);
+    var addonAlpha = newAddonTextbox;
+    var newAddonTextbox = document.createElement('input');
+    newAddonTextbox.placeholder = bp;
+    newAddonTextbox.className = "form-control";
+    newAddonTextbox.id = "block" + curid + "-beta";
+    newAddonPanel.appendChild(newAddonTextbox);
+    var addonBeta = newAddonTextbox;
+    var newAddonTextbox = document.createElement('input');
+    newAddonTextbox.placeholder = cp;
+    newAddonTextbox.className = "form-control";
+    newAddonTextbox.id = "block" + curid + "-gamma";
+    newAddonPanel.appendChild(newAddonTextbox);
+    var addonGamma = newAddonTextbox;
+
+        newAddonPanelSettingsButton.onclick = function() {
+        if(newAddon._flag) {
+            addonAlpha.style.visibility = "hidden";
+            addonBeta.style.visibility = "hidden";
+            addonGamma.style.visibility = "hidden";
+        } else {
+            addonAlpha.style.visibility = "visible";
+            addonBeta.style.visibility = "visible";
+            addonGamma.style.visibility = "visible";           
+        }
+        newAddon._flag = !newAddon._flag;
+        }
+    document.getElementById("statemachine-demo").appendChild(newAddon);
+    window.jsp = instance;
+    var windows = jsPlumb.getSelector("#" + "block" + curid);
+    instance.draggable(windows);
+    instance.batch(function () {
+        instance.makeSource(windows, {
+            filter: ".ep",
+            anchor: "Continuous",
+            connector: [ "StateMachine", { curviness: 10 } ],
+            connectorStyle: { strokeStyle: "#333333", lineWidth: 1, outlineColor: "transparent", outlineWidth: 10 },
+            maxConnections: 5,
+            onMaxConnections: function (info, e) {
+                alert("Maximum connections (" + info.maxConnections + ") reached");
+            }
+        });
+        instance.makeTarget(windows, {
+            dropOptions: { hoverClass: "dragHover" },
+            anchor: "Continuous",
+            allowLoopback: true
+        });
+            jsPlumb.fire("jsPlumbDemoLoaded", instance);
+    });
+    jsPlumb.fire("jsPlumbDemoLoaded", instance);
+
+    curid++;
+
+}
+
+function bufToString(b) {
+    var r = "";
+    for(var i in b) {
+        r += String.fromCharCode(b[i]);
+    }
+    return r;
+}
+
+function main() {
+  var schemeDiv = document.getElementById("statemachine-demo");
+  socket = io.connect(':8889');
+  socket.on('event', function (data) {
+    if(data.data.length == 4) {        
+        document.getElementById(data.ip + "-prefix").innerHTML = bufToString(data.data);
+    } else { 
+        showEvent(data.data,data.id);
+    }
+    document.getElementById(data.ip + "-row").className = "info";
+    setTimeout(function() {
+        document.getElementById(data.ip + "-row").className = "success";        
+    }, 50);
+  });
+  socket.on('client', function(data) {
+    document.getElementById('clients').innerHTML += '<tr id="' + data.ip + '-row" class="success"><td>' + data.ip + '</td><td id="' + data.ip + '-prefix"></td><td id="' + data.ip + '-state">ok</td><td><button type="button" class="btn btn-default btn-xs" data-toggle="button" aria-pressed="false" autocomplete="off">экран</button><button type="button" class="btn btn-default btn-xs" data-toggle="button" aria-pressed="false" autocomplete="off">звук</button></td><td><button type="button" class="btn btn-default btn-xs" onclick="installScheme(\'' + data.ip + '\')"><span class="glyphicon glyphicon-upload"></span></button></td></tr>';
+  });
+  socket.on('state', function (data) {
+    switch(data.state) {
+        case "error":
+            document.getElementById(data.ip + "-row").className = "danger";
+            document.getElementById(data.ip + "-state").innerHTML = "error";
+            break;
+    }
+  });
+}
+
+function getRandomInt(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function parseScheme() {
+    var model = {};
+    var blocksNumber=0;
+    var connections = [];
+    var buffer = [];
+    $.each(instance.getConnections(), function (idx, connection) {
+        connections.push({
+            from: connection.sourceId,
+            to: connection.targetId,
+            out: connection.getOverlay('label').getLabel().split(" ")[2],
+            inp: connection.getOverlay('label').getLabel().split(" ")[0],
+        });
+    });
+    $("#statemachine-demo .w").each(function (idx, elem) {
+        blocksNumber++;
+        var $elem = $(elem);
+        var flag=0;
+        var setting_alpha = document.getElementById($elem.attr('id') + "-alpha").value;
+        var setting_beta = document.getElementById($elem.attr('id') + "-beta").value;
+        var setting_gamma = document.getElementById($elem.attr('id') + "-gamma").value;
+        buffer.push(elem._type);
+        buffer.push(Math.floor(setting_alpha/0x100)); buffer.push(setting_alpha%0x100);
+        buffer.push(Math.floor(setting_beta/0x100)); buffer.push(setting_beta%0x100);
+        buffer.push(Math.floor(setting_gamma/0x100)); buffer.push(setting_gamma%0x100);
+        flag=0;
+        for(var i in connections) {
+            if((connections[i].to.split("block").join("") == $elem.attr('id').split("block").join(""))&&(connections[i].out=="A")) {
+                switch(connections[i].inp) {
+                    case "A":
+                        buffer.push(1);
+                        break;
+                    case "B":
+                        buffer.push(2);
+                        break;
+                    case "C":
+                        buffer.push(3);
+                        break;
+                }
+                buffer.push(parseInt(connections[i].from.split("block").join("")));               
+                flag=1;
+            }
+        }
+        if(flag==0) {
+            buffer.push(0); buffer.push(255);
+        }
+        flag=0;
+        for(var i in connections) {
+            if((connections[i].to.split("block").join("") == $elem.attr('id').split("block").join(""))&&(connections[i].out=="B")) { 
+                switch(connections[i].inp) {
+                    case "A":
+                        buffer.push(1);
+                        break;
+                    case "B":
+                        buffer.push(2);
+                        break;
+                    case "C":
+                        buffer.push(3);
+                        break;
+                }
+                buffer.push(parseInt(connections[i].from.split("block").join(""))); 
+                flag=1;             
+            }
+        }
+        if(flag==0) {
+            buffer.push(0); buffer.push(255);
+        }
+        flag=0;
+        for(var i in connections) {
+            if((connections[i].to.split("block").join("") == $elem.attr('id').split("block").join(""))&&(connections[i].out=="C")) { 
+                switch(connections[i].inp) {
+                    case "A":
+                        buffer.push(1);
+                        break;
+                    case "B":
+                        buffer.push(2);
+                        break;
+                    case "C":
+                        buffer.push(3);
+                        break;
+                }
+                buffer.push(parseInt(connections[i].from.split("block").join(""))); 
+                flag=1;             
+            }
+        }
+        if(flag==0) {
+            buffer.push(0); buffer.push(255);
+        }
+    }); 
+    /*model = {
+        b: blocks,
+        c: connections
+    }
+    outstr += "BLK\n\n";
+    for(var i in model.b) {
+        outstr += model.b[i].id + "\n";
+        outstr += model.b[i].left + "\n";
+        outstr += model.b[i].top + "\n";
+        outstr += model.b[i].alpha + "\n";
+        outstr += model.b[i].beta + "\n";
+        outstr += model.b[i].gamma + "\n\n";
+    }
+    outstr += "CON\n\n";
+    for(var i in model.c) {
+        outstr += model.c[i].from + "\n";
+        outstr += model.c[i].to + "\n";
+        outstr += model.c[i].out + "\n";
+        outstr += model.c[i].inp + "\n";
+    }
+    var modelStr = JSON.stringify(model);*/
+    buffer.unshift(blocksNumber % 0x100);
+    buffer.unshift(Math.floor(blocksNumber/0x100));
+    return buffer;
+}
+
+function installScheme(a) {
+    socket.emit('scheme-install',{scheme: parseScheme(), ip: a});
+}
+
+function initOsc() {
+    oscScreen = $.plot($("#oscilloscope"), [[0,0]]);
+}
+
+function showEvent(ev,id) {
+    var d = [];
+    var options = {
+    series: {
+        lines: {
+            show: true,
+            fill: true,
+            fillColor: { colors: [{ opacity: 0.7 }, { opacity: 0.1}] }
+        }
+    },
+    colors: ["#FF7070", "#0022FF"],
+    grid: {
+        hoverable: true
+    }       
+    }
+    //alert(ev[14]*0x100+ev[15]);
+    for(var i=0;i<(ev[14]*0x100+ev[15])*2;i+=2) {
+        d.push([(ev[ev.length-(ev[14]*0x100+ev[15])*2*2+i]*0x100+ev[ev.length-(ev[14]*0x100+ev[15])*2*2+i+1])*Math.pow(10,-ev[16]), (ev[ev.length-(ev[14]*0x100+ev[15])*2+i]*0x100+ev[ev.length-(ev[14]*0x100+ev[15])*2+i+1])*Math.pow(10,-ev[17])]);
+    }
+    //alert(ev);
+
+    if(oscPlay==true) {
+        oscScreen = $.plot($("#oscilloscope"), [d], options);
+    }
+    var time = ev[0] + "." + ev[1] + "." + ev[2] + " " + ev[3] + ":" + ev[4] + ":" + ev[5] + ":" + (ev[6]*0x100 + ev[7]*1) + ":" + (ev[8]*0x100 + ev[9]*1) + ":" + (ev[10]*0x100 + ev[11]*1);
+    var prefixLength = ev[12]*0x100 + ev[13]*1;
+    var prefixBuffer = ev.slice(18,18+prefixLength);
+    var prefix = "";
+    for(var i in prefixBuffer) prefix+=String.fromCharCode(prefixBuffer[i]);
+    //alert(prefixBuffer);
+    logEvent(id,prefix,time);
+}
+
+function logEvent(id,prefix,time) {
+    document.getElementById("eventLog").innerHTML = "<tr><td>" + id + "</td><td>" + prefix + "</td><td>" + time + "</td></tr>" + document.getElementById("eventLog").innerHTML;
+}
+
+function getRandomArbitary(min, max)
+{
+  return Math.random() * (max - min) + min;
+}
+
+
+function openOsc() {
+    document.getElementById('oscill-container').style.visibility = "visible";
+    document.getElementById('constructor-navbar').style.visibility = "hidden";
+}
+
+function closeOsc() {
+    document.getElementById('oscill-container').style.visibility = "hidden";
+    document.getElementById('constructor-navbar').style.visibility = "visible";
+}
+
+function setMode(m) {
+        $("#btnConstructor").removeClass("active");
+        $("#btnScreen").removeClass("active");
+        $("#btnLog").removeClass("active");
+        $("#btnSettings").removeClass("active");
+        document.getElementById('oscill-container').style.visibility = "hidden";
+        document.getElementById('constructor-navbar').style.visibility = "hidden";
+        document.getElementById('log-container').style.visibility = "hidden";
+        document.getElementById('settings-container').style.visibility = "hidden";
+    switch(m) {
+        case "screen":
+            document.getElementById('oscill-container').style.visibility = "visible";
+            $("#btnScreen").addClass("active");
+            break;
+        case "constructor":
+            document.getElementById('constructor-navbar').style.visibility = "visible";
+            $("#btnConstructor").addClass("active"); 
+            break;
+        case "log":
+            document.getElementById('log-container').style.visibility = "visible";
+            $("#btnLog").addClass("active");
+            break;
+        case "settings":
+            document.getElementById('settings-container').style.visibility = "visible";
+            $("#btnSettings").addClass("active");
+            break;
+    }
+}
+
+function deleteDisconnected() {
+    $('#clients').children('tr').each(function (i,o) {
+        if(o.className=="danger") {
+            $(o).remove();
+        }
+    });
+}
---
+++ main.js
@@ -0,0 +1,143 @@
+var http = require("http"),
+    url = require("url"),
+    path = require("./node_modules/path"),
+    net = require('net'),
+    io = require("socket.io"),
+    express = require("express"),
+    fs = require("fs"),
+    vm = require("vm")
+    port = process.argv[2] || 80;
+
+var state = 0;
+var scheme = {};
+var eventLog = [];
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  var contentTypesByExtension = {
+    '.html': "text/html",
+    '.css':  "text/css",
+    '.js':   "text/javascript"
+  };
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var headers = {};
+      var contentType = contentTypesByExtension[path.extname(filename)];
+      if (contentType) headers["Content-Type"] = contentType;
+      response.writeHead(200, headers);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("HTTP Server started @ http://localhost:" + port + "/");
+
+var app = express();
+var server = http.createServer(app);
+var io = io.listen(server);
+server.listen(8889);
+
+console.log("Socket server started @ http://localhost:8889/");
+
+io.sockets.on('connection', function (socket) {
+
+  for(var i in clients) {
+    socket.emit('client',{ip: clients[i].name});
+    socket.emit('state',{ip: clients[i].name, state: clients[i].state});
+  }
+
+  socket.on('play', function (data) {
+    console.log("[PLAY]\n");
+    state = 0;
+  });
+
+  socket.on('stop', function (data) {
+    console.log("[STOP]\n");
+    state = 2;    
+  });
+
+  socket.on('pause', function (data) {
+    if(state==2) {
+      console.log("[ERR] Can't pause if stopped\n"); 
+      state=1
+    } else {     
+      console.log("[PAUSE]\n");
+    }
+  });
+
+  socket.on('refresh', function (data) {
+      scheme = JSON.parse(data);
+      console.log("Scheme refreshed\n");
+  });
+
+  socket.on('scheme-install', function (data) {
+      for(var i in clients) {
+        console.log(data);
+        if(clients[i].name == data.ip) {
+          clients[i].write(new Buffer(data.scheme));  
+        }
+      }
+  });
+
+});
+
+// TCP Server for data
+
+var clients = [];
+ 
+net.createServer(function (socket) {
+  console.log("Connected!");
+  socket.bufferSize = 10000; // WARNING
+  socket.name = socket.remoteAddress + ":" + socket.remotePort
+  socket._evt = new Object();
+  socket._evt.data = new Array();
+  clients.push(socket);
+  io.sockets.emit("client",{ip:socket.name});
+  socket.on('data', function (data) {
+    var d = data.toJSON();
+    for(var i in d.data) socket._evt.data.push(d.data[i]);
+    if((d.data[d.data.length-1]==0xFF)&&(d.data[d.data.length-2]==0xFF)&&(d.data[d.data.length-3]==0xFF)&&(d.data[d.data.length-4]==0xFF)) {
+      socket._evt.ip = socket.name;
+      socket._evt.data = socket._evt.data.slice(0,socket._evt.data.length-4);
+      eventLog.push(socket._evt);
+      socket._evt.id = eventLog.length-1;
+      io.sockets.emit("event",socket._evt);
+      delete socket._evt.data;
+      delete socket._evt;
+      socket._evt = new Object();
+      socket._evt.data = new Array();
+    }
+  });
+  socket.on('end', function () {
+    console.log("END");
+    clients.splice(clients.indexOf(socket), 1);
+  });
+  socket.on('error', function(err) {
+    console.log("ERR");
+    io.sockets.emit("state",{ip:socket.name,state:"error"});
+    clients.splice(clients.indexOf(socket), 1);
+  });
+}).listen(5001);
+
+console.log("TCP server started @ http://localhost:5001/");
