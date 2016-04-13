489af295e1b4b69a4d5d4f953544efb7166f193c
Alexander <aleksander@MacBook-Pro.local>
2016-04-13 20:48:28 +0300

    net
    Former-commit-id: d938aca17fa16356f181baa67113fc63474c1da6 [formerly 1b3cf4de86e46c252c9d096b9304bfad0d58a8a8]
    Former-commit-id: 658ff1bc41569553e1bd3aa4c872e21d6653916f

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const http = require('http');
 const https = require("https");
 const net = require("net");
 const bodyParser  = require('body-parser');
@@ -7,9 +8,11 @@ const jwt = require('jsonwebtoken');
 const kernel = require("./modules/kernel.js");
 var io = require("socket.io");
 
+const security_settings = JSON.parse(fs.readFileSync('./security/users.json'))
+const users = security_settings['users']
 const key  = fs.readFileSync('./security/nevod-key.pem');
 const cert = fs.readFileSync('./security/nevod-cert.pem');
-var token_secret = "nevod11secret22key8-0-$0"
+var token_secret = security_settings['token_secret']
 var https_options = {
     key: key,
     cert: cert
@@ -29,12 +32,11 @@ kernel.init(task)
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const port = (process.env.PORT || 3000);
+const port = (process.env.PORT || 443);
 const https_server = https.createServer(https_options,app).listen(port);
 app.set('port',port);
 app.use(express.static(__dirname + '/public/'));
 app.get('/', function(req,res){
-  console.log('get')
 	res.sendFile(__dirname + '/public/index.html');
 });
 
@@ -43,17 +45,25 @@ router.get('/',function(req,res){
   res.json({ message: 'Welcome to the coolest API on earth! Oops you might be waiting for something else.' });
 })
 router.post('/authenticate',function(req,res){
-  var token = jwt.sign(req.body,token_secret,{
-    exexpiresInMinutes: 1440
+  var accept = false
+  users.forEach(function(user){
+    if(user['name']==req.body.user && user['password']==req.body.pass){
+      accept = true
+      return
+    }
   })
-  res.json({success: true,token: token});
+  if(accept){
+    var token = jwt.sign(req.body,token_secret,{exexpiresInMinutes:1440})
+    res.json({success:accept,token:token})
+  } else {
+    res.json({success:accept})
+  }
 })
 app.use('/api',router)
-// var server = app.listen(app.get('port'),function(){})
 var io = io.listen(https_server);
 kernel.io = io
 
-// console.log(task+" server started @ http://localhost:"+app.get('port')+"/");
+console.log("Node app started, HTTPS server: @ https://localhost:"+app.get('port')+"/");
 
 io.sockets.on('connection', function (socket) {
 
@@ -91,5 +101,58 @@ io.sockets.on('connection', function (socket) {
 
 });
 
+//net socket
+var msg = '{"jsonrpc":"2.0","method":"sum","params":{},"id":"2"}'
+console.log(msg)
+var options = {
+  hostname:"192.168.1.189",
+  port:8383,
+  path:'/',
+  method:"POST",
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': msg.length
+  }
+}
 
-//net server-client
+var req =http.request(options,function(res){
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.')
+  })
+})
+
+req.on('error',function(err){
+  console.log("error: "+err.message)
+})
+
+req.write(msg)
+req.end()
+
+// var socket = new net.Socket()
+// // var serv = net.createServer(3000)
+// var PORT = 2222
+// var HOST = "192.168.1.189"
+//
+//
+// // serv.listen(3000,function(){
+// //   console.log("tcp listening 3000")
+// // })
+// //
+// // serv.on('connection',function(){
+// //   console.log("someone connected")
+// // })
+//
+// socket.connect(PORT,"192.168.1.189",function(){
+//   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+//   socket.write("hello")
+// })
+//
+// socket.on('data',function(data){
+//   console.log(data.toString())
+// })
--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 
+var auth_lodaing = false;
+var token = null;
+
 window.onbeforeunload = function(){
 	window.scrollTo(0,0);
 }
-$(document).ready(function(){})
+$(document).ready(function(){
+	console.log(localStorage)
+})
 
 $('.main-container').scroll(function(){
   if($('.main-container').scrollTop()>=190){
@@ -30,26 +35,35 @@ var openAuthModal = function(){
 }
 
 var authenticate = function(){
+	if(auth_lodaing){
+		return
+	}
+	$('#id_auth-warn-empty').css('display','none')
+	$('#id_auth-warn-warn').css('display','none')
 	var pass = $('#id_password_input').val()
 	var user = $('#id_username_input').val()
 	if(pass.length==0 || user.length==0){
 		$('#id_auth-warn-empty').css('display','block')
 	} else {
+		auth_lodaing=true;
 		$.post('/api/authenticate',{user:user,pass:pass},function(answer){
-			console.log(answer)
+			auth_lodaing=false;
+			$('#id_auth-loader').css('display','none')
+			if(answer.success){
+				localStorage.setItem("access-token")
+			} else {
+				$('#id_auth-warn-warn').css('display','block')
+			}
 		})
-		$('#id_auth-modal-footer').css('display','none')
 		$('#id_auth-warn-empty').css('display','none')
-		$('#id_auth').css('display','none')
 		$('#id_auth-loader').css('display','block')
 	}
 }
 
-var closeAuthModal = function(){//delete
-	$('#id_auth-modal-footer').css('display','block')
-	$('#id_auth').css('display','block')
+var closeAuthModal = function(){
 	$('#id_auth-loader').css('display','none')
 	$('#id_auth-warn-empty').css('display','none')
+	$('#id_auth-warn-warn').css('display','none')
 	$('#id_password_input').val("")
 	$('#id_username_input').val("")
 }
