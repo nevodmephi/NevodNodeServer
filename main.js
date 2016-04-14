2dd377c4571f1baa43fac08da4fc60a8c8375de8
Alexander <aleksander@MacBook-Pro.local>
2016-04-14 14:46:02 +0300

    fix
    Former-commit-id: bc71a1431fba4d4f232ad70de6e4cefeffa5e094 [formerly 9022f72770d4b2995a60ff35c3156db681eab394]
    Former-commit-id: 4adf3f2c3fc438ee10bec036144aa1a03b8d0ae6

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-var io = require("socket.io"),
-    express = require("express")
+const express = require("express");
+const nevod = require("nevod");
+var io = require("socket.io");
 
-const task = (process.env.TASK || "eas")
-if(task != "eas" && task != "neutron"){
-  console.log(task+" - is unkown task environment variable, use ONLY: 'eas' or 'neutron' for TASK ")
+const task = (process.env.TASK || "all")
+if(task != "eas" && task != "neutron" && task != "all"){
+  console.log(task+" - is unkown task environment variable, use ONLY: 'eas' or 'neutron' or 'all' for TASK ")
   process.exit()
 }
-const kernel = require("./modules/kernel.js")
-kernel.init(task)
-kernel.load()
+
+var kernel = nevod.initServerKernel(task)
 
 //http server
 var app = express();
-const port = (process.env.PORT || 80);
+const port = (process.env.PORT || 443);tt
 app.set('port',port);
-app.use(express.static(__dirname + '/public/'+task));
+app.use(express.static(__dirname + '/public/'));
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/public/'+task+'/'+task+'.html');
 });
@@ -22,7 +22,7 @@ var server = app.listen(app.get('port'),function(){})
 var io = io.listen(server);
 kernel.io = io
 
-console.log(task+" server started @ http://localhost:"+app.get('port')+"/");
+console.log("Node app started, HTTP server: @ https://localhost:"+app.get('port')+"/");
 
 io.sockets.on('connection', function (socket) {
 
@@ -46,16 +46,72 @@ io.sockets.on('connection', function (socket) {
   })
 
   socket.on('db-col-names',function(data){
-    kernel.getDBCollections(data,function(cols){
-      socket.emit('db-col-names-list',cols)
-    })
+    // kernel.mongo.getDBCollections(data,function(cols){
+    //   socket.emit('db-col-names-list',cols)
+    // })
   })
 
   socket.on('db-get',function(data){
-    kernel.getDataFromDB(data,function(db_data){
-      socket.emit(data.res,db_data)
-      db_data=null
-    })
+    // kernel.mongo.getDataFromDB(data,function(db_data){
+    //   socket.emit(data.res,db_data)
+    //   db_data=null
+    // })
   })
 
 });
+
+//net socket
+// var msg = '{"jsonrpc":"2.0","method":"maxz","params":{"first":2,"second":3},"id":"2"}'
+// console.log(msg)
+// var options = {
+//   hostname:"192.168.1.189",
+//   port:8383,
+//   path:'/',
+//   method:"POST",
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded',
+//     'Content-Length': msg.length
+//   }
+// }
+//
+// var req =http.request(options,function(res){
+//   console.log(`STATUS: ${res.statusCode}`);
+//   console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+//   res.setEncoding('utf8');
+//   res.on('data', (chunk) => {
+//     console.log(`BODY: ${chunk}`);
+//   });
+//   res.on('end', () => {
+//     console.log('No more data in response.')
+//   })
+// })
+//
+// req.on('error',function(err){
+//   console.log("error: "+err.message)
+// })
+//
+// req.write(msg)
+// req.end()
+
+// var socket = new net.Socket()
+// // var serv = net.createServer(3000)
+// var PORT = 2222
+// var HOST = "192.168.1.189"
+//
+//
+// // serv.listen(3000,function(){
+// //   console.log("tcp listening 3000")
+// // })
+// //
+// // serv.on('connection',function(){
+// //   console.log("someone connected")
+// // })
+//
+// socket.connect(PORT,"192.168.1.189",function(){
+//   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+//   socket.write("hello")
+// })
+//
+// socket.on('data',function(data){
+//   console.log(data.toString())
+// })
