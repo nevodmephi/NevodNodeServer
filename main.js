a9b2b09a5ae927b83c942933b52c1f855012beeb
Alexander <aleksander@MacBook-Pro.local>
2016-04-14 21:40:46 +0300

    update1
    Former-commit-id: d07983845ec380daa3e9a16f96e3e819060b1302 [formerly 65e81374e83b99464f9fa2e42280acf70b6d7c5d]
    Former-commit-id: c65145ffbfb335fcb68588fc2bce24ce58e60525

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var kernel = nevod.initServerKernel(task)
 var app = express();
 const port = (process.env.PORT || 80);
 app.set('port',port);
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public/'+task));
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/public/'+task+'/'+task+'.html');
 });
@@ -59,59 +59,3 @@ io.sockets.on('connection', function (socket) {
   })
 
 });
-
-//net socket
-// var msg = '{"jsonrpc":"2.0","method":"maxz","params":{"first":2,"second":3},"id":"2"}'
-// console.log(msg)
-// var options = {
-//   hostname:"192.168.1.189",
-//   port:8383,
-//   path:'/',
-//   method:"POST",
-//   headers: {
-//     'Content-Type': 'application/x-www-form-urlencoded',
-//     'Content-Length': msg.length
-//   }
-// }
-//
-// var req =http.request(options,function(res){
-//   console.log(`STATUS: ${res.statusCode}`);
-//   console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-//   res.setEncoding('utf8');
-//   res.on('data', (chunk) => {
-//     console.log(`BODY: ${chunk}`);
-//   });
-//   res.on('end', () => {
-//     console.log('No more data in response.')
-//   })
-// })
-//
-// req.on('error',function(err){
-//   console.log("error: "+err.message)
-// })
-//
-// req.write(msg)
-// req.end()
-
-// var socket = new net.Socket()
-// // var serv = net.createServer(3000)
-// var PORT = 2222
-// var HOST = "192.168.1.189"
-//
-//
-// // serv.listen(3000,function(){
-// //   console.log("tcp listening 3000")
-// // })
-// //
-// // serv.on('connection',function(){
-// //   console.log("someone connected")
-// // })
-//
-// socket.connect(PORT,"192.168.1.189",function(){
-//   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-//   socket.write("hello")
-// })
-//
-// socket.on('data',function(data){
-//   console.log(data.toString())
-// })
